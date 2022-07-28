0329c476bc919fc412ca076979c058dd82a8376f
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-07-29 03:17:51 +0530

    small fixes

--- vulncheck/dependency_check/node_cve.py
+++ vulncheck/dependency_check/node_cve.py
@@ -23,7 +23,7 @@ def check_node_cve(code):
 
     for file in package_lock:
         f = open(file, "r")
-        data = load(f)
+        data = json.load(f)
         payload = json.dumps({
             "name": "test",
             "version": "0.0.1",
